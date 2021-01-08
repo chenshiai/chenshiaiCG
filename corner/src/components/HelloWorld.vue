13b1c4e6ea087629531c554999afaad2d452122a
chenshuai <chenshuai@youzan.com>
2021-01-08 15:52:36 +0800

    feat: 添加配置

--- corner/src/components/HelloWorld.vue
+++ corner/src/components/HelloWorld.vue
@@ -1,19 +1,34 @@
 <template>
+  <div class="a">
+    <div class="b"></div>
+  </div>
   <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
   },
   data() {
     return {
-      count: 0
-    }
+      count: 0,
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+.a {
+  width: 1px;
+
+  .b {
+    background: red;
   }
 }
-</script>
+</style>
